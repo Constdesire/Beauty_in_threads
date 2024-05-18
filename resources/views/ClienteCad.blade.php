414e77019274d57caec35c928934923a98015723
Desirée Constantino <117848187+Constdesire@users.noreply.github.com>
2024-05-18 01:24:00 -0300

    Add files via upload

--- resources/views/ClienteCad.blade.php
+++ resources/views/ClienteCad.blade.php
@@ -1,71 +1,194 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> 
-    <title>Cadastrar Cliente</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <title>Cadastre-se</title>
+    <style>
+        body {
+            background-color: #A36A4F;
+        }
+        .header-site{
+    background-image: url(img/cad.png);
+    background-position: center top;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    color: #2f302f;
+    padding: 12% 10%;
+}
+    </style>
 </head>
 <body>
-<div class="container">
-  <div class="row align-items-start">
-    <div class="col">
-          <nav class="navbar navbar-expand bg-primary">
-              <div class="container-fluid">
-                  <a class="navbar-brand text-light" href="#" >Cadastrar Cliente</a>
-                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                       <span class="navbar-toggler-icon"></span>
-                     </button>
-                          <div class="collapse navbar-collapse" id="navbarNav">
-                                <ul class="navbar-nav">
-                                 <li class="nav-item">
-                                  <a class="nav-link active text-light" aria-current="page" href="#">Cadastrar</a>
-                                 </li>
-                                    <li class="nav-item">
-                                       <a class="nav-link text-light" href="#">Consultar</a>
-                                    </li>
-                               </ul>
-                         </div>
-                  </div>
+<nav class="relative px-4 py-4 flex justify-between items-center bg-white">
+        <a class="mr-auto text-3xl font-bold leading-none" href="#">
+            <img class="h-12 navbar-logo" alt="logo" src="img/Logo.png">
+        </a>
+        <div class="lg:hidden">
+            <button class="navbar-burger flex items-center text-pink-600 p-3">
+                <svg class="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <title>Mobile menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                </svg>
+            </button>
+        </div>
+        <ul class="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
+		<li><a class="text-sm text-black-600 hover:text-gray-500" href="welcome.blade.php">Home</a></li>
+            <li class="text-gray-300">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
+                </svg>
+            </li>
+            <li><a class="text-sm text-black-600 hover:text-gray-500" href="welcome.blade.php/#Serviços">Nossos serviços </a></li>
+            <li class="text-gray-300">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
+                </svg>
+            </li>
+            <li><a class="text-sm text-black-400 hover:text-gray-500" href="welcome.blade.php/#Sobre">Sobre Nos</a></li>
+            <li class="text-gray-300">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
+                </svg>
+            </li>
+            <li><a class="text-sm text-black-400 hover:text-gray-500" href="welcome.blade.php/#Contato">Contato</a></li>
+            <li class="text-gray-300">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" class="w-4 h-4 current-fill" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v0m0 7v0m0 7v0m0-13a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"></path>
+                </svg>
+            </li>
+            <li><a class="text-sm text-black-400 hover:text-gray-500" href="">Horários Marcados</a></li>
+        </ul>
+    </nav>
+    <div class="navbar-menu relative z-50 hidden">
+        <div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+        <nav class="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+            <div class="flex items-center mb-8">
+                <a class="mr-auto text-3xl font-bold leading-none" href="#">
+                    <img class="h-12 lg:hidden" alt="logo" src="img/Logo.png">
+                </a>
+                <button class="navbar-close">
+                    <svg class="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                </button>
             </div>
-           <p> 
-           <p align="left"><font size="5">Cadastrar -  Agendamento de Clientes</p></font>
-           <p align="left"><font size="4">Sistema utilizado para agedamento de serviços</font></p>
-           
-<div class="container mt-5">
-        <form method="POST" action="/adicionarCliente">
-            @csrf
-           <div class="form-group mb-2">
-                <label for="formGroupExampleInput2">Nome</label>
-                    <input type="text" class="form-control" name="Nome" >
-           </div>
-           <div class="form-group mb-2">
-                <label for="formGroupExampleInput2" >Data de Nascimento</label>
-                    <input type="text" class="form-control" name="DataNasc" placeholder="XXXX/XX/XX" >
-           </div>
-           <div class="form-group mb-2">
-                <label for="formGroupExampleInput2">CPF</label>
-                    <input type="text" class="form-control" name="CPF" placeholder="XXXX/XX/XX">
-           </div>
-           <div class="form-group mb-2">
-                <label for="formGroupExampleInput2">Telefone</label>
-                    <input type="text" class="form-control" name="Telefone" placeholder="(XX)XXXXX-XXXX">
-           </div>
-           <div class="form-group mb-2">
-                <label for="formGroupExampleInput2">Endereco</label>
-                    <input type="text" class="form-control" name="Endereco" placeholder="">
-           </div>
-           <div class="form-group mb-2">
-                <label for="formGroupExampleInput2">CEP</label>
-                    <input type="text" class="form-control" name="CEP" placeholder="XXXX/XX/XX">
-           </div>
-           <div class="form-group mb-2">
-                <label for="formGroupExampleInput2">Email</label>
-                    <input type="text" class="form-control" name="Email" placeholder="">
-           </div>
-           <button type="submit" class="btn btn-primary">Cadastrar</button> 
-      </form>
-</div>
+            <div>
+                <ul>
+                    <li class="mb-1">
+					<a class="block p-4 text-sm font-semibold text-black-600 hover:text-gray-500" href="welcome.blade.php">Home</a>
+                    </li>
+                    <li class="mb-1">
+					<a class="block p-4 text-sm font-semibold text-black-600 hover:text-gray-500" href="welcome.blade.php">Nossos Serviços</a>
+                    </li>
+                    <li class="mb-1">
+					<a class="block p-4 text-sm font-semibold text-black-600 hover:text-gray-500" href="welcome.blade.php">Sobre nós</a>
+                    </li>
+                    <li class="mb-1">
+					<a class="block p-4 text-sm font-semibold text-black-600 hover:text-gray-500" href="welcome.blade.php">Contato</a>
+                    </li>
+                </ul>
+            </div>
+                <p class="my-4 text-xs text-center text-gray-400">
+                    <span>Copyright © 2024</span>
+                </p>
+            </div>
+        </nav>
+    </div>
+    <br>
+     <!--------------------->
+     <section class="header-site">
+    
+    </section>
+    <!----------form---------------->
+    <br><br>
+    <h1 class="text-3xl font-semibold mb-6 text-black text-center">Cadastre-se!!!</h1>
+      <p class="my-4 text-center">Faça seu cadastro para se tornar um cliente do Beleza em fios!</p>
+    <div class="flex items-center justify-center p-12">
+    <!-- Author: FormBold Team -->
+        <form>
+            <div class="mb-5">
+                <label for="name" class="mb-3 block text-base font-medium text-[#07074D]">
+                    Full Name
+                </label>
+                <input type="text" name="name" id="name" placeholder="Full Name"
+                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+            </div>
+            <div class="mb-5">
+                <label for="phone" class="mb-3 block text-base font-medium text-[#07074D]">
+                    Phone Number
+                </label>
+                <input type="text" name="phone" id="phone" placeholder="Enter your phone number"
+                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+            </div>
+            <div class="mb-5">
+                <label for="email" class="mb-3 block text-base font-medium text-[#07074D]">
+                    Email Address
+                </label>
+                <input type="email" name="email" id="email" placeholder="Enter your email"
+                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+            </div>
+            <div class="-mx-3 flex flex-wrap">
+                <div class="w-full px-3 sm:w-1/2">
+                    <div class="mb-5">
+                        <label for="date" class="mb-3 block text-base font-medium text-[#07074D]">
+                            Date
+                        </label>
+                        <input type="date" name="date" id="date"
+                            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                    </div>
+                </div>
+                <div class="w-full px-3 sm:w-1/2">
+                    <div class="mb-5">
+                        <label for="time" class="mb-3 block text-base font-medium text-[#07074D]">
+                            Time
+                        </label>
+                        <input type="time" name="time" id="time"
+                            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-5 pt-3">
+                <label class="mb-5 block text-base font-semibold text-[#07074D] sm:text-xl">
+                    Address Details
+                </label>
+                <div class="-mx-3 flex flex-wrap">
+                    <div class="w-full px-3 sm:w-1/2">
+                        <div class="mb-5">
+                            <input type="text" name="area" id="area" placeholder="Enter area"
+                                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
+                    </div>
+                    <div class="w-full px-3 sm:w-1/2">
+                        <div class="mb-5">
+                            <input type="text" name="city" id="city" placeholder="Enter city"
+                                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
+                    </div>
+                    <div class="w-full px-3 sm:w-1/2">
+                        <div class="mb-5">
+                            <input type="text" name="state" id="state" placeholder="Enter state"
+                                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
+                    </div>
+                    <div class="w-full px-3 sm:w-1/2">
+                        <div class="mb-5">
+                            <input type="text" name="post-code" id="post-code" placeholder="Post Code"
+                                class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+            <button type="submit" class="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300">Cadastrar</button>
+            </div>
+        </form>
+    </div>
+
 </body>
 </html>
