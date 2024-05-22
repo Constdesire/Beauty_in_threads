312b01a85277e37cd4bdf07d922e0b2a2e42ee44
Isaanine <111399296+Isaanine@users.noreply.github.com>
2024-05-22 00:52:57 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () { return view('welcome');});
 Route::get('/login.blade.php', function () { return view('login');});
 Route::get('/Adm.blade.php', function () { return view('adm');});
+Route::get('/AgendamentoCad.blade.php', function () { return view('AgendamentoCad');});
+
 
 
 Route::get("/ClienteCad", [ClienteController::class, "ClienteCad"]);
